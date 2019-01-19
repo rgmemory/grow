bb0ee5e45ad642ab750f1551fd4418b2286d50f5
Russell Memory <rmemory@russells-mbp-2.lan>
2019-01-19 12:28:08 -0700

    Able to get a list of residents, need to add them to original object

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
-// let peopleArray = []
 
 const port = 3005;
 
@@ -10,101 +9,154 @@ let app = express();
 app.use(bodyParser.json());
 
 let promises = [];
+let planets = [];
 let people = [];
 let peopleArray = [];
+let planetPromises = [];
+let itemArray = [];
+let axiosPlanetArray = [];
+let axiosResults = [];
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
+
+//Gets a list of people using a loop that dynamically adjusts to the number of people the API makes available.
 app.get("/people", (req, res) => {
-    let peopleArray = []
+  let peopleArray = [];
 
-    axios.get('https://swapi.co/api/people').then(result => {
-  
-        for(let i = 0; i < result.data.results.length; i++){
-            peopleArray.push(result.data.results[i])
+  axios.get("https://swapi.co/api/people").then(result => {
+    let loopCounter = Math.ceil(result.data.count / 10) + 1;
+
+    for (let i = 0; i < result.data.results.length; i++) {
+      peopleArray.push(result.data.results[i]);
+    }
+
+    for (let i = 2; i < loopCounter; i++) {
+      let tempString = `https://swapi.co/api/people/?page=${i}`;
+      promises.push(axios.get(tempString));
+    }
+
+    Promise.all(promises).then(results => {
+      results.forEach(item => {
+        people.push(item.data.results);
+      });
+      for (let i = 0; i < people.length; i++) {
+        for (let j = 0; j < people[i].length; j++) {
+          peopleArray.push(people[i][j]);
         }
-
-        for (let i = 2; i < 10; i++) {
-            let tempString = `https://swapi.co/api/people/?page=${i}`;
-            promises.push(axios.get(tempString));
-          }
-        
-          Promise.all(promises).then(results => {
-              results.forEach(item => {        
-                    people.push(item.data.results)
-              })
-              for(let i = 0; i < people.length; i++){
-                  for(let j = 0; j < people[i].length; j++){
-                      peopleArray.push(people[i][j])
-                  }
-              }
-              res.send(peopleArray)
-          })
-    })
+      }
+      res.send(peopleArray);
+    });
+  });
 });
 
+//Sorts the people from the API according to name, height, and mass.
 app.get("/people/:sortBy", (req, res) => {
   let peopleArray = [];
 
   let param = req.params.sortBy;
 
-  axios.get(`https://swapi.co/api/people`).then(result => {
-    peopleArray = result.data.results;
-
-    if (param === "name") {
-      let name = peopleArray.slice(0);
-      name.sort((a, b) => {
-        var x = a.name.toLowerCase();
-        var y = b.name.toLowerCase();
-        return x < y ? -1 : x > y ? 1 : 0;
-      });
-      res.send(name);
-    } else if (param === "height") {
-      let height = peopleArray.slice(0);
-      height.sort(function(a, b) {
-        return a.height - b.height;
-      });
-      res.send(height);
-    } else if (param === "mass") {
-      let mass = peopleArray.slice(0);
-      mass.sort(function(a, b) {
-        return a.mass - b.mass;
-      });
-      res.send(mass);
+  axios.get("https://swapi.co/api/people").then(result => {
+    for (let i = 0; i < result.data.results.length; i++) {
+      peopleArray.push(result.data.results[i]);
     }
+
+    for (let i = 2; i < 10; i++) {
+      let tempString = `https://swapi.co/api/people/?page=${i}`;
+      promises.push(axios.get(tempString));
+    }
+
+    Promise.all(promises).then(results => {
+      results.forEach(item => {
+        people.push(item.data.results);
+      });
+      for (let i = 0; i < people.length; i++) {
+        for (let j = 0; j < people[i].length; j++) {
+          peopleArray.push(people[i][j]);
+        }
+      }
+
+      if (param === "name") {
+        let name = peopleArray.slice(0);
+        name.sort((a, b) => {
+          var x = a.name.toLowerCase();
+          var y = b.name.toLowerCase();
+          return x < y ? -1 : x > y ? 1 : 0;
+        });
+        res.send(name);
+      } else if (param === "height") {
+        let height = peopleArray.slice(0);
+        height.sort(function(a, b) {
+          return a.height - b.height;
+        });
+        res.send(height);
+      } else if (param === "mass") {
+        let mass = peopleArray.slice(0);
+        mass.sort(function(a, b) {
+          return a.mass - b.mass;
+        });
+        res.send(mass);
+      }
+    });
   });
 });
 
+
+
+//Gets a list of planets using a loop that dynamically adjusts to the number of planets the API makes available.
 app.get("/planets", (req, res) => {
-  axios.get("https://swapi.co/api/planets/").then(result => {
-    console.log(result.data);
+  let planetsArray = [];
 
-    res.send(result.data.results);
+  axios.get("https://swapi.co/api/planets").then(result => {
+    let loopCounter = Math.ceil(result.data.count / 10) + 1;
+
+    for (let i = 0; i < result.data.results.length; i++) {
+      planetsArray.push(result.data.results[i]);
+    }
+
+    for (let i = 2; i < loopCounter; i++) {
+      let tempString = `https://swapi.co/api/planets/?page=${i}`;
+      promises.push(axios.get(tempString));
+    }
+
+    Promise.all(promises).then(results => {
+      results.forEach(item => {
+        planets.push(item.data.results);
+      });
+      for (let i = 0; i < planets.length; i++) {
+        for (let j = 0; j < planets[i].length; j++) {
+          planetsArray.push(planets[i][j]);
+        }
+      }
+
+      for (let x = 0; x < planetsArray.length; x++) {
+        console.log(planetsArray[x].name, "----", x + 1);
+        let residentCounter = 0;
+
+        for (let i = 0; i < planetsArray[x].residents.length; i++) {
+          residentCounter++;
+
+          let tempString = planetsArray[x].residents[i];
+
+          axiosPlanetArray.push(axios.get(tempString));
+        }
+
+        if (axiosPlanetArray.length >= 1) {
+          Promise.all(axiosPlanetArray).then(results => {
+            results.forEach((item, index) => {
+              //////////////////I'm able to retrive the residents of each planet and console.log them but I'm still working on getting them in the correct order
+              //////////////////and replacing them in data that I send back to the user
+              console.log(item.data.name);
+            });
+          });
+        }
+
+        axiosPlanetArray = [];
+      }
+
+      res.send(planetsArray);
+    });
   });
 });
-
-// setTimeout(message, 4000)
-
-// run a loop for each number of pages there are
-
-// in the loop have a settimeout function that calls the api with the correct number url
-
-// app.get('/people', (req, res) => {
-//     for(let i = 2; i < 5; i++){
-//         axios.get(`https://swapi.co/api/people/?page=${i}`).then(result => {
-//             // console.log('-----------------------------------------------', result.data)
-//             people.push(result.data)
-
-//             console.log('people(-----------------------------------------------', people)
-
-//             // setTimeout(() => {
-//                 //         people = result.data
-//                 //     }, 0)
-//             })
-//         }
-//         res.send(people)
-
-//     // console.log('people', people)
-// })
